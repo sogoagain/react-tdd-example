e03a09038c081a55c90618e65ef7601032a93a79
sogoagain <imyong0@gmail.com>
2020-12-03 01:01:54 +0900

    Implement - App renders the expected wait time

--- src/containers/StatusBoardContainer.jsx
+++ src/containers/StatusBoardContainer.jsx
@@ -3,7 +3,12 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export default function StatusBoardContainer() {
-  const { customer } = useSelector((state) => state);
+  const { customer, waitTime } = useSelector((state) => state);
 
-  return <h2>[띵동] {customer} 고객님</h2>;
+  return (
+    <>
+      <h2>[띵동] {customer} 고객님</h2>
+      <h3>예상 대기 시간: {waitTime}초</h3>
+    </>
+  );
 }
