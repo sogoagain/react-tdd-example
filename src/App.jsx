3936712dbaa0179417174423ea46e5c8f4a112cd
sogoagain <imyong0@gmail.com>
2020-12-03 00:41:30 +0900

    Implement - App renders greetings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+import Title from "./components/Title";
+
 export default function App() {
-  return <h1>iPortfolio 은행</h1>;
+  return (
+    <>
+      <Title />
+      <p>고객님, 오늘도 행복한 하루 되세요. ^^</p>
+    </>
+  );
 }
